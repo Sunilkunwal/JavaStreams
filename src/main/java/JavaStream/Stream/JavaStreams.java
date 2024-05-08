1dd72353be6d5b000624c3817617fbce7b91faf7
Sunilkunwal <sunilkunwal97@gmail.com>
2024-05-08 16:40:44 +0530

    first

---
+++ src/main/java/JavaStream/Stream/JavaStreams.java
@@ -0,0 +1,33 @@
+package JavaStream.Stream;
+
+
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+@Test
+public class JavaStreams {
+
+	
+	public void regular() {
+		// Count the number of names starting with alphabet A in list
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Abhijeet");
+		names.add("Don");
+		names.add("Alekhya");
+		names.add("Adam");
+		names.add("Ram");
+		int count = 0;
+
+		for (int i = 0; i < names.size(); i++) {
+			String actual = names.get(i);
+			if (actual.startsWith("A")) {
+				count++;
+			}
+
+		}
+
+		System.out.println(count);
+	}
+
+}
